5f7040e1d588b8126cc9c7d32dad370aab8b8084
Tchacorom Sandrine <doudouvi.sand@gmail.com>
2022-10-05 10:26:27 +0000

    Page404

--- front/src/pages/Page404.js
+++ front/src/pages/Page404.js
@@ -1,5 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
 function Page404 () {
-
+    return (
+        <div className="404">
+            <div className="page404">
+                <h1>404</h1>
+                    <p>
+                        <span>Oups! La page que</span>vous demandez n'existe pas.
+                    </p>
+            </div>
+            <Link to ="/" className="linkt">
+                Retournez sur la page d'accueil
+            </Link>
+        </div>
+    )
 }
 
 export default Page404;
